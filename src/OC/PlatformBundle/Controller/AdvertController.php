0e3257fdf7a334e3dd848fbe720d35534c8f2d1e
Gregoire <canugregoire@gmail.com>
2018-05-04 12:31:11 +0200

    Entités

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use OC\PlatformBundle\Entity\Advert;
 
 class AdvertController extends Controller {
 
@@ -53,14 +54,23 @@ class AdvertController extends Controller {
     // On donne à cette méthode l'argument $id, pour
     // correspondre au paramètre {id} de la route
     public function viewAction($id) {
-        $advert = array(
-            'title' => 'Recherche développpeur Symfony2',
-            'id' => $id,
-            'author' => 'Alexandre',
-            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-            'date' => new \Datetime()
-        );
 
+        // On récupère le repository
+        $repository = $this->getDoctrine()
+                ->getManager()
+                ->getRepository('OCPlatformBundle:Advert')
+        ;
+
+        // On récupère l'entité correspondante à l'id $id
+        $advert = $repository->find($id);
+
+        // $advert est donc une instance de OC\PlatformBundle\Entity\Advert
+        // ou null si l'id $id  n'existe pas, d'où ce if :
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id " . $id . " n'existe pas.");
+        }
+
+        // Le render ne change pas, on passait avant un tableau, maintenant un objet
         return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
                     'advert' => $advert
         ));
@@ -83,29 +93,32 @@ class AdvertController extends Controller {
     }
 
     public function addAction(Request $request) {
-        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
-        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-//        if ($request->isMethod('POST')) {
-            // Ici, on s'occupera de la création et de la gestion du formulaire
-            
-            // On récupère le service
-            $antispam = $this->container->get('oc_platform.antispam');
+        // Création de l'entité
+        $advert = new Advert();
+        $advert->setTitle('Recherche développeur Symfony.');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
+        // On peut ne pas définir ni la date ni la publication,
+        // car ces attributs sont définis automatiquement dans le constructeur
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
 
-            // Je pars du principe que $text contient le texte d'un message quelconque
-            $text = '...';
-            if ($antispam->isSpam($text)) {
-                throw new \Exception('Votre message a été détecté comme spam !');
-            }
+        // Étape 1 : On « persiste » l'entité
+        $em->persist($advert);
 
-            // Ici le message n'est pas un spam
+        // Étape 2 : On « flush » tout ce qui a été persisté avant
+        $em->flush();
+
+        // Reste de la méthode qu'on avait déjà écrit
+        if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
             // Puis on redirige vers la page de visualisation de cettte annonce
-            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
-//        }
+            return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+        }
 
         // Si on n'est pas en POST, alors on affiche le formulaire
-        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', array('advert' => $advert));
     }
 
     public function editAction($id, Request $request) {
